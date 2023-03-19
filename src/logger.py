049a085b594db95a189a7f980090fe80854d4045
Sabeel <syedsabeel88@gmail.com>
2023-03-19 18:04:17 +0400

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,34 @@
+import logging
+import os
+from datetime import datetime
+from exception import error_message_detail
+
+Log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+Log_path = os.path.join(os.getcwd(),"logs", Log_file)
+os.makedirs(Log_path,exist_ok=True)
+
+Log_file_path = os.path.join(Log_path,Log_file)
+
+
+logging.basicConfig(
+    filename=Log_file_path,format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s -%(message)s",
+    level=logging.INFO
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
