0b05ca59d08ed142452cf4079f1d1242a5cc63ae
Sabeel <syedsabeel88@gmail.com>
2023-03-23 15:53:21 +0400

    Data Transformation

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.Components.data_transformation import DataTransformation
+
 # decorator dataclass is used to define the variables inside rather using init
 @dataclass
 class DataIngestionConfig:
@@ -48,4 +50,7 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data =obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transform(train_data,test_data)
